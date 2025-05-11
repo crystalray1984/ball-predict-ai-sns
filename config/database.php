9138ff86ce4a156bdd5584fea83478bd27c76e80
Crystal <crystalray@qq.com>
2025-05-11 23:35:37 +0800

    init

--- config/database.php
+++ config/database.php
@@ -31,7 +31,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
